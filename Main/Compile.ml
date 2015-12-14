72be5af6493d73112b0033a5436655a47b36c050
sdusson <samuel.dusson@telecom-bretagne.eu>
2015-12-14 17:27:46 +0100

    Changement dans Compile.ml pour tester parser de classe

--- Main/Compile.ml
+++ Main/Compile.ml
@@ -4,6 +4,6 @@ let execute lexbuf verbose =
   print_endline "Parsing...";
   (*print_endline (ParseClass.classe LexClass.nexttoken lexbuf )*)
     
-  let exp = ParseExpr.expression LexExpr.nexttoken lexbuf in
-  print_endline (Expr.string_of_expr exp)
+  let exp = ParseClass.classe LexClass.nexttoken lexbuf in
+  print_endline (exp)
   
