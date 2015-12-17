5b844b972e1bcd3f1bf1c525cad2f0a0660c4e9a
sdusson <samuel.dusson@telecom-bretagne.eu>
2015-12-17 18:30:50 +0100

    mis a jour astclass

--- Parsing/astClass.ml
+++ Parsing/astClass.ml
@@ -1,7 +1,18 @@
+
+type modifierAccess = 
+  | Public | Protected | Private | Empty
+
 type classAst =
   {
     classename : string;
+    access : modifierAccess;
   }
 
-let printClassAst c = print_endline(c.classename)
+let string_of_modifieraccess c = match c with
+  | Public -> "public"
+  | Protected -> "protected"
+  | Private -> "private"
+  | Empty -> ""
+
+let printClassAst c = print_endline(  string_of_modifieraccess(c.access) ^ c.classename)
 
