bebb07d0f1859f64a8d93374ea347d3d5dad2cf3
Chloebrz <chloe.brouzes@hotmail.fr>
2016-02-08 16:40:59 +0100

    Small fix

--- Phase2/Typing/CheckAST.ml
+++ Phase2/Typing/CheckAST.ml
@@ -1,14 +1,14 @@
 open AST
 
-let string_type_of_infix_op = function
-  | Op_add | Op_sub | Op_mul | Op_div | Op_mod -> "int"
-
 let stringOf_prim = function
-  | Some(Type.Primitive(Type.Int)) -> "int"
-  | Some(Type.Primitive(Type.Char)) -> "char"
   | Some(Type.Primitive(Type.Boolean)) -> "boolean"
-  | Some(Type.Ref({tpath=[]; tid="String"})) -> "string"
+  | Some(Type.Primitive(Type.Char)) -> "char"
+  | Some(Type.Primitive(Type.Byte)) -> "byte"
+  | Some(Type.Primitive(Type.Short)) -> "short"
+  | Some(Type.Primitive(Type.Int)) -> "int"
+  | Some(Type.Primitive(Type.Int)) -> "long"
   | Some(Type.Primitive(Type.Float)) -> "float"
+  | Some(Type.Ref({tpath=[]; tid="String"})) -> "string"
 
 exception Wrong_types_aop of Type.t option * assign_op * Type.t option
 
