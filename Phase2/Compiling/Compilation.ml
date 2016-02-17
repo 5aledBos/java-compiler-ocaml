4004539021f704e18e64a0980c04bed659484a90
sdusson <samuel.dusson@telecom-bretagne.eu>
2016-02-18 00:45:38 +0100

    adds classes in classdescritortable and adds methods in classDescriptors

--- Phase2/Compiling/Compilation.ml
+++ Phase2/Compiling/Compilation.ml
@@ -9,8 +9,8 @@ type objectDescriptor =
 
 type classDescriptor =
 {
-	name : string;
-	methodList : string list
+	cdname : string;
+	cdmethods : (string, string) Hashtbl.t
 }
 
 type globalClassDescriptor =
@@ -22,7 +22,7 @@ type globalClassDescriptor =
 
 type globalData = 
 {
-  methodTable : (string, string) Hashtbl.t;
+  methodTable : (string, astmethod) Hashtbl.t;
   classDescriptorTable : (string, globalClassDescriptor) Hashtbl.t
 }
 
@@ -38,7 +38,7 @@ let addPredifinedClassesToDescriptorTable descriptorTable =
 
 let printClassDescriptorTable cdtable =
   print_endline("list of the classes");
-  Hashtbl.iter (fun key value -> print_string(key)) cdtable
+  Hashtbl.iter (fun key value -> print_endline(key)) cdtable
 
 let printMethodTable mtable =
   print_endline("list of the methods of all classes");
@@ -49,14 +49,25 @@ add method, addattribute
 
 *)
 
-let compile fileAst = 
+let addMethodsToClassDesciptor cd m=
+  Hashtbl.add cd.cdmethods m.mname (cd.cdname ^ "_" ^ m.mname)
+
+let addMethodsAndAttributesToDescriptors classDescriptorTable ttype = match ttype.info with
+ | Class c -> let cd = { cdname = ttype.id; cdmethods = Hashtbl.create 20  } in
+      List.iter (addMethodsToClassDesciptor cd) c.cmethods; Hashtbl.add classDescriptorTable cd.cdname (ClassDescriptor(cd))
+ | Inter -> ()
+
+
+let compile ast = 
   let compilationData = { methodTable = Hashtbl.create 20; classDescriptorTable = Hashtbl.create 20 }
   in
   addPredifinedClassesToDescriptorTable compilationData.classDescriptorTable;
+  List.iter (addMethodsAndAttributesToDescriptors compilationData.classDescriptorTable) ast.type_list;
+(*  match fileAst with*)
+(*  | { package = p ; type_list = cl } -> *)
   compilationData
 
 let printCompilationData data = match data with
   | {methodTable = mtable; classDescriptorTable = cdtable } -> printMethodTable(mtable); printClassDescriptorTable(cdtable)
-
 
   
